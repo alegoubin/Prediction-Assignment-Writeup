baac25dfcae1faf08441691698dd3fdf40b2e696
alegoubin <alegoubin.public@gmail.com>
2016-02-28 01:12:56 +0100

    Added files via upload

--- plot1.R
+++ plot1.R
@@ -1,7 +1,32 @@
 #functions for each plot, width and height are used to specifiy the canvas size for the saved file
 #the data frame must be passed as an argument as well
 
-buildPlot1 <- function (df,width,height) {
+buildPlot1 <- function (wd,width,height) {
+  
+  # set working directorw with source file and saved plots
+  setwd(wd)
+  
+  ## Read source data
+  fullData <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
+  
+  ## Convert fields into correct format
+  fullData$Date<-as.Date(fullData$Date,format="%d/%m/%Y")
+  fullData$Time<-strptime(x = as.character(fullData$Time),format = "%H:%M:%S")
+  fullData$Time<- format(fullData$Time, "%H:%M:%S")
+  
+  ##subset data
+  fullData <- fullData[(fullData$Date=="2007-02-01") | (fullData$Date=="2007-02-02"),]
+  
+  ## Convert fields into correct format
+  fullData$Global_active_power <- as.numeric(as.character(fullData$Global_active_power))
+  fullData$Global_reactive_power<- as.numeric(as.character(fullData$Global_reactive_power))
+  fullData$Voltage<- as.numeric(as.character(fullData$Voltage))  
+  fullData$Global_intensity <- as.numeric(as.character(fullData$Global_intensity))
+  fullData$Sub_metering_1 <- as.numeric(as.character(fullData$Sub_metering_1))    
+  fullData$Sub_metering_2 <- as.numeric(as.character(fullData$Sub_metering_2))   
+  fullData$Sub_metering_3 <- as.numeric(as.character(fullData$Sub_metering_3))    
+  
+  df <- fullData
   
   #sets a 1 grid
   par(mfrow = c(1,1))
--- plot3.R
+++ plot3.R
@@ -1,4 +1,29 @@
-buildPlot3 <- function (df,width,height) {
+buildPlot3 <- function (wd,width,height)  {
+  
+  # set working directorw with source file and saved plots
+  setwd(wd)
+  
+  ## Read source data
+  fullData <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
+  
+  ## Convert fields into correct format
+  fullData$Date<-as.Date(fullData$Date,format="%d/%m/%Y")
+  fullData$Time<-strptime(x = as.character(fullData$Time),format = "%H:%M:%S")
+  fullData$Time<- format(fullData$Time, "%H:%M:%S")
+  
+  ##subset data
+  fullData <- fullData[(fullData$Date=="2007-02-01") | (fullData$Date=="2007-02-02"),]
+  
+  ## Convert fields into correct format
+  fullData$Global_active_power <- as.numeric(as.character(fullData$Global_active_power))
+  fullData$Global_reactive_power<- as.numeric(as.character(fullData$Global_reactive_power))
+  fullData$Voltage<- as.numeric(as.character(fullData$Voltage))  
+  fullData$Global_intensity <- as.numeric(as.character(fullData$Global_intensity))
+  fullData$Sub_metering_1 <- as.numeric(as.character(fullData$Sub_metering_1))    
+  fullData$Sub_metering_2 <- as.numeric(as.character(fullData$Sub_metering_2))   
+  fullData$Sub_metering_3 <- as.numeric(as.character(fullData$Sub_metering_3))    
+  
+  df <- fullData
   
   #sets a 1 grid
   par(mfrow = c(1,1))
